b6f2a3ceb80c9edd793bf222fa9cb06cd4b40923
sophyaw <sophyaww17@gmail.com>
2024-10-16 10:45:45 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan6/pages/introduction_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sophy Aw',
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.w600,
+              fontStyle: FontStyle.italic,
+              fontFamily: 'Serif'),
+          bodyLarge: TextStyle(color: Colors.black87),
+          bodyMedium: TextStyle(color: Colors.black54),
+          bodySmall: TextStyle(color: Colors.black45),
+        ),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.w600,
+              fontStyle: FontStyle.italic,
+              fontFamily: 'Serif'),
+          bodyLarge: TextStyle(color: Colors.white70),
+          bodyMedium: TextStyle(color: Colors.white60),
+          bodySmall: TextStyle(color: Colors.white54),
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      home: const IntroductionPage(),
+    );
+  }
+}
---
+++ lib/pages/introduction_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:pertemuan6/pages/homepage.dart';
+
+class IntroductionPage extends StatelessWidget {
+  const IntroductionPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      next: const Text("Selanjutnya"),
+      done: const Text("Selesai"),
+      onDone: () {
+        Navigator.of(context).pop();
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return const MyHomePage();
+            },
+          ),
+        );
+      },
+      pages: [
+        PageViewModel(
+          title: "The First Page",
+          body: "Number 1",
+          image: Image.network("https://picsum.photos/800/500"),
+        ),
+        PageViewModel(
+          title: "The Second Page",
+          body: "Number 2",
+          image: Image.network("https://picsum.photos/800/600"),
+        ),
+        PageViewModel(
+          title: "The Third Page",
+          body: "Number 3",
+          image: Image.network("https://picsum.photos/900/700"),
+        ),
+      ],
+    );
+  }
+}
